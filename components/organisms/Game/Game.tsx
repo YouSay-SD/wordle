b23784bdb13269e41fb13c2eb782eab4f9f1b776
YouSay <yousay.sd@gmail.com>
2023-06-24 17:55:02 -0300

    2-style: add game styles and keyboard mobile styles

--- components/organisms/Game/Game.tsx
+++ components/organisms/Game/Game.tsx
@@ -4,13 +4,16 @@ import Keyboard from '@/components/molecules/Keyboard/Keyboard'
 import Board from '@/components/molecules/Board/Board'
 import Button from '@/components/atoms/Button/Button'
 import { useWordle } from '@/hooks/useWordle'
+import className from 'classnames/bind'
+import styles from './Game.module.scss'
+const cx = className.bind(styles)
 
 const Game: FC = () => {
   const word = 'yousay'
   const { slots, columnsQuantity, keySelected, resetGame } = useWordle({ word })
 
   return (
-    <div>
+    <div className={cx('game')}>
       <Board slots={slots} col={columnsQuantity} />
       <Keyboard keySelected={keySelected} />
       <Button buttonText='!JUGAR¡' adaText='!JUGAR¡' onClick={resetGame} />
