f01dda543e58cdbb731f436fe771c8057eb69ba5
YouSay <yousay.sd@gmail.com>
2023-06-24 17:11:30 -0300

    2-feature: create slot actions

---
+++ helpers/deleteSlot.ts
@@ -0,0 +1,25 @@
+import { SlotProps, StatusProps } from '@/components/atoms/Slot/Slot.interface'
+import { STATUS } from '@/constants/status'
+
+type deleteSlotProps = {
+  slots: SlotProps[],
+  indexPosition: number,
+}
+
+export const deleteSlot = ({ slots, indexPosition }: deleteSlotProps) => {
+  const slotsUpdated = slots.map((slot: SlotProps) => {
+    const indexValidation = (indexPosition - 1)
+
+    return slot.index === indexValidation
+      ? {
+          ...slot,
+          value: '',
+          status: STATUS.EMPTY as StatusProps
+        }
+      : { ...slot }
+  })
+
+  return {
+    slotsUpdated
+  }
+}
