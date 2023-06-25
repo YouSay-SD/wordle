76a44a965d33c40b66d95443a7eb5307cd6074c1
YouSay <yousay.sd@gmail.com>
2023-06-25 19:20:26 -0300

    12-feature: refactor hooks

--- hooks/useKeyboard.ts
+++ hooks/useKeyboard.ts
@@ -1,38 +1,28 @@
-import { useContext, useEffect, useState } from 'react'
-import { useCreateSlots } from './useCreateSlots'
+import { useEffect, useState } from 'react'
 import { BACKSPACE_KEY, ENTER_KEY, KEYBOARD_SET } from '@/constants/keyboardSet'
-import { actionRemoveSlot } from '@/helpers/deleteSlot'
-import { actionAddSlot } from '@/helpers/addSlot'
-import { actionValidateSlots, validateSlots } from '@/helpers/validateSlots'
-import { SlotProps } from '@/components/atoms/Slot/Slot.interface'
-import { STATUS } from '@/constants/status'
-import { WordleContext } from '@/context/wordleContext'
+import { useWordle } from './useWordle'
 
 export interface UseWordleProps {
   isGameStarted: boolean
 }
 
-export const useKeyboard = ({ isGameStarted }: UseWordleProps) => {
+export interface UseKeyboardProps {
+  keySelected: string;
+  resetCompleteGame: () => void;
+}
+
+export const useKeyboard = (): UseKeyboardProps => {
   const {
     slots,
-    victory,
-    plays,
-    score,
     resetGame,
     indexPosition,
-    setIndexPosition,
     limitBackPosition,
-    setLimitBackPosition,
-    columnsQuantity,
-    wordMaped,
     removeSlot,
     addSlot,
     randomWord,
-    validateSlots
-  } = useContext(WordleContext)
-  console.log('indexPosition', indexPosition)
+    isGameStarted
+  } = useWordle()
   const [keySelected, setKeySelected] = useState<string>('')
-  const [typed, setTypes] = useState<string>('')
 
   // Reset Game
   const resetCompleteGame = () => {
@@ -53,62 +43,27 @@ export const useKeyboard = ({ isGameStarted }: UseWordleProps) => {
         if (isKeyAllowed) {
           const wasEnterPressed = key === ENTER_KEY
           const wasBackspacePressed = key === BACKSPACE_KEY
-          const currentSlot = slots.find(({ index }) => index === indexPosition)
 
           // Back Slot
           if (wasBackspacePressed && indexPosition > limitBackPosition) {
             removeSlot()
-            setIndexPosition((prev) => prev - 1)
           }
 
           setKeySelected(key)
 
           // Next Slot
           if (!wasBackspacePressed && !wasEnterPressed && indexPosition < slots.length) {
-            addSlot({ key })
-            setIndexPosition((prev) => prev + 1)
-
-            if (currentSlot?.position === columnsQuantity) {
-              setLimitBackPosition(limitBackPosition + columnsQuantity)
-              // console.log('validateee')
-              // Validate Slots
-              validateSlots()
-
-              const valuesInRange = slots.slice(limitBackPosition, columnsQuantity + limitBackPosition).map(({ value }) => value)
-              const concatenatedValue = valuesInRange.join('')
-              const isMatch = concatenatedValue === randomWord
-              if (isMatch) {
-                victory()
-                resetGame()
-              }
-            }
+            addSlot({ indexPosition, key })
           }
         }
       }
     }
-    // console.log('slots', slots)
+
     window.addEventListener('keydown', onKeyPress)
     return () => {
       window.removeEventListener('keydown', onKeyPress)
     }
-  }, [columnsQuantity, indexPosition, keySelected, limitBackPosition, slots, randomWord, wordMaped, isGameStarted, typed, resetGame, removeSlot, setIndexPosition, setLimitBackPosition, victory, addSlot, validateSlots])
-
-  useEffect(() => {
-    // Game Finished
-    const slotsSolved = !slots.some(({ status }) => status === STATUS.EMPTY)
-
-    if (slotsSolved) {
-      plays()
-    }
-  }, [plays, slots])
-
-  useEffect(() => {
-    const wasEnterPressed = keySelected === ENTER_KEY
-    const wasBackspacePressed = keySelected === BACKSPACE_KEY
-    if (!wasBackspacePressed && !wasEnterPressed) {
-      setTypes((prev) => prev + keySelected)
-    }
-  }, [keySelected])
+  }, [addSlot, indexPosition, isGameStarted, limitBackPosition, removeSlot, slots.length])
 
   return { keySelected, resetCompleteGame }
 }
