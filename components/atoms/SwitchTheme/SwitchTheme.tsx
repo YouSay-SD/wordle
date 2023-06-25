c72541776fa7ec431657f4918c1abbb82c976b9b
YouSay <yousay.sd@gmail.com>
2023-06-25 01:33:40 -0300

    11-feature: create SwitchTheme atom

---
+++ components/atoms/SwitchTheme/SwitchTheme.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useTheme } from 'next-themes'
+import className from 'classnames/bind'
+import styles from './SwitchTheme.module.scss'
+const cx = className.bind(styles)
+
+const SwitchTheme = () => {
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
+  return (
+    <button onClick={toggleTheme} className={cx('switch-theme')}>
+      <div className={`${cx('circle')} circle-theme`} />
+    </button>
+  )
+}
+
+export default SwitchTheme
