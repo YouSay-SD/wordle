276bd57f8bb41237c46b166d2c2d35911cd8cf5e
YouSay <yousay.sd@gmail.com>
2023-06-25 19:20:48 -0300

    12-feature: create WordleContext

---
+++ context/WordleContext.initial.ts
@@ -0,0 +1,26 @@
+import { WordleContextProps } from './WordleContext.interface'
+
+export const initialState: WordleContextProps = {
+  slots: [],
+  setSlots: () => {},
+  score: {
+    plays: -1,
+    victories: 0
+  },
+  wordMaped: undefined,
+  timeRemaining: 0,
+  formattedTime: '',
+  indexPosition: 0,
+  limitBackPosition: 0,
+  columnsQuantity: undefined,
+  isGameStarted: false,
+  setIsGameStarted: () => {},
+  setLimitBackPosition: () => {},
+  setIndexPosition: () => {},
+  resetGame: () => {},
+  validateSlots: () => {},
+  removeSlot: () => {},
+  addSlot: () => {},
+  victoryScore: () => {},
+  playsScore: () => {}
+}
--- context/types.js
+++
@@ -1,5 +0,0 @@
-export const types = {
-  VALIDATE_SLOTS: '[Slot] Validate Slots',
-  ADD_SLOT: '[Slot] Add Slot',
-  REMOVE_SLOT: '[Slot] Remove Slot'
-}
