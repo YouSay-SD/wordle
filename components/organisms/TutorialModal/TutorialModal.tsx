8badd8716c099b866d0db51368a91079bafc5aec
YouSay <yousay.sd@gmail.com>
2023-06-25 20:40:48 -0300

    12-feature: update TutorialModal markup

--- components/organisms/TutorialModal/TutorialModal.tsx
+++ components/organisms/TutorialModal/TutorialModal.tsx
@@ -61,7 +61,7 @@ const TutorialModal: FC<TutorialModalProps> = ({ setIsGameStarted, setIsOpen, is
         </div>
 
         <div className={cx('examples')}>
-          <P>Ejemplos</P>
+          <P className={cx('example-title')}>Ejemplos</P>
           <div className={cx('example-items')}>
             <Slot status={STATUS.SUCCESS as StatusProps} value='G' position={1} index={0} />
             <Slot status={STATUS.EMPTY as StatusProps} value='A' position={1} index={0} />
@@ -69,7 +69,7 @@ const TutorialModal: FC<TutorialModalProps> = ({ setIsGameStarted, setIsOpen, is
             <Slot status={STATUS.EMPTY as StatusProps} value='O' position={1} index={0} />
             <Slot status={STATUS.EMPTY as StatusProps} value='S' position={1} index={0} />
           </div>
-          <P>La letra G está en la palabra y en la posición correcta.</P>
+          <P>La letra <span>G</span> está en la palabra y en la posición correcta.</P>
 
           <div className={cx('example-items')}>
             <Slot status={STATUS.EMPTY as StatusProps} value='V' position={1} index={0} />
@@ -78,7 +78,7 @@ const TutorialModal: FC<TutorialModalProps> = ({ setIsGameStarted, setIsOpen, is
             <Slot status={STATUS.EMPTY as StatusProps} value='A' position={1} index={0} />
             <Slot status={STATUS.EMPTY as StatusProps} value='L' position={1} index={0} />
           </div>
-          <P>La letra C está en la palabra pero en la posición incorrecta.</P>
+          <P>La letra <span>C</span> está en la palabra pero en la posición incorrecta.</P>
 
           <div className={cx('example-items')}>
             <Slot status={STATUS.EMPTY as StatusProps} value='C' position={1} index={0} />
@@ -87,14 +87,14 @@ const TutorialModal: FC<TutorialModalProps> = ({ setIsGameStarted, setIsOpen, is
             <Slot status={STATUS.EMPTY as StatusProps} value='T' position={1} index={0} />
             <Slot status={STATUS.FILLED as StatusProps} value='O' position={1} index={0} />
           </div>
-          <P>La letra O no está en la palabra.</P>
+          <P>La letra <span>O</span> no está en la palabra.</P>
         </div>
 
         <div className={cx('explanation')}>
           <P>Puede haber letras repetidas. Las pistas son independientes para cada letra.</P>
         </div>
 
-        <P className={cx('center-text')}>¡Una palabra nueva cada 5 minutos!</P>
+        <P className={cx('center-text', 'last-paragraph')}>¡Una palabra nueva cada 5 minutos!</P>
         <Button className={cx('button')} buttonText='!JUGAR¡' adaText='!JUGAR¡' onClick={closeModal} />
       </div>
     </Modal>
